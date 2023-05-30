a916830ac982822f65cdbab1efe7236ec7b1d8bd
mohdumar1654 <mohd.umar1654@gmail.com>
2023-05-30 22:42:41 +0530

    Update

---
+++ Assignments/Day 52/BinarySearch.java
@@ -0,0 +1,28 @@
+public class BinarySearch {
+    public static void main(String[] args) {
+
+        // Here we have implemented Binary Search on a Sorted array
+        // Notes Link :
+        // https://scaler-production-new.s3.ap-southeast-1.amazonaws.com/attachments/attachments/000/045/134/original/Adv_BS1.pdf?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIDNNIRGHAQUQRWYA%2F20230530%2Fap-southeast-1%2Fs3%2Faws4_request&X-Amz-Date=20230530T162124Z&X-Amz-Expires=561600&X-Amz-SignedHeaders=host&X-Amz-Signature=7d71aec688846427b5f11a2dcf50061315e4c8515c1cc527e6a972a5ab3cbe5e
+
+        int[] arr = { 11, 12, 13, 14, 5, 6, 7, 8, 9, 10 };
+        int k = 13;
+        int low = 0;
+        int high = arr.length - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] == k) {
+                System.out.print(mid);
+                break;
+            }
+            if (arr[mid] < k) {
+                low = mid + 1;
+            }
+            if (arr[mid] > k) {
+                high = mid - 1;
+            }
+        }
+        System.out.print(-1);
+    }
+}
