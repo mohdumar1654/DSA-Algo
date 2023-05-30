e200dbd7202c83604935d2f1b1d2acea809cd7a8
mohdumar1654 <mohd.umar1654@gmail.com>
2023-05-30 22:43:33 +0530

    Update

--- Assignments/Day 52/BinarySearch.java
+++ Assignments/Day 52/BinarySearch.java
@@ -5,7 +5,7 @@ public class BinarySearch {
         // Notes Link :
         // https://scaler-production-new.s3.ap-southeast-1.amazonaws.com/attachments/attachments/000/045/134/original/Adv_BS1.pdf?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIDNNIRGHAQUQRWYA%2F20230530%2Fap-southeast-1%2Fs3%2Faws4_request&X-Amz-Date=20230530T162124Z&X-Amz-Expires=561600&X-Amz-SignedHeaders=host&X-Amz-Signature=7d71aec688846427b5f11a2dcf50061315e4c8515c1cc527e6a972a5ab3cbe5e
 
-        int[] arr = { 11, 12, 13, 14, 5, 6, 7, 8, 9, 10 };
+        int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
         int k = 13;
         int low = 0;
         int high = arr.length - 1;
